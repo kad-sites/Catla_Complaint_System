526ce8fc625a032471c2d348efd67d1332054200
Zoheb <zoheb@example.com>
2026-08-14 02:44:27 +0530

    Fix: Android App Back Button ClassCastException crash

--- catla-tech-app/app/src/main/java/com/example/catlatechapp/MainActivity.kt
+++ catla-tech-app/app/src/main/java/com/example/catlatechapp/MainActivity.kt
@@ -5,30 +5,33 @@ import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        val webView = WebView(this)
+        webView = WebView(this)
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
         
         val webSettings: WebSettings = webView.settings
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true
         
         // Load the technician portal
-        webView.loadUrl("http://192.168.0.244:3000/technician")
+        webView.loadUrl("https://catla-complaint-system.vercel.app/technician")
         
         setContentView(webView)
     }
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
-        val webView = findViewById<WebView>(android.R.id.content)
-        if (webView != null && webView.canGoBack()) {
+        if (this::webView.isInitialized && webView.canGoBack()) {
             webView.goBack()
         } else {
             super.onBackPressed()
